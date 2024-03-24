8cfd8360567ae495e705b766f6a909fb2094c42d
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-24 18:48:07 +0100

    Add typing to observer hook

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -1,12 +1,16 @@
 import { useCallback, useRef, DependencyList } from "react";
 
+//this hook will be used to generate a reference for the IntersectionObserver
+//the reference should be applied to the last element of the list
+//as soon as the reference is seen by the observer, the specified callback will be fired
+
 export default function useIntersectionObserver<T extends HTMLElement>(
   callback: () => void,
   deps: DependencyList
 ) {
-  const ref = useRef(null);
+  const observer = useRef<IntersectionObserver | null>(null);
 
-  const useCallback(()=>{},[])
+  const ref = useCallback(() => {}, []);
 
   return ref;
 }
