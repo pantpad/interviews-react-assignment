db56329b815f7085e3b060e9df70256a799073a3
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-24 17:48:59 +0100

    Add fetchProducts endpoint function

---
+++ src/utils/endpoits.ts
@@ -0,0 +1,12 @@
+let productAPI = "/products";
+
+export async function fetchProducts(page: number = 0, limit: number = 10) {
+  const response = await fetch(`/products?page=${page}&limit=${limit}`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+  return data;
+}
