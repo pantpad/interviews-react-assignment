d580599d07d3106925df694f44c059fe49875a64
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-24 17:58:31 +0100

    Add page limit parameters for fetchFn

--- src/hooks/useFetchProducts.tsx
+++ src/hooks/useFetchProducts.tsx
@@ -2,7 +2,9 @@ import { Product, ProductResponse } from "../types/ProductType";
 import { useEffect, useState } from "react";
 
 export default function useFetchProducts(
-  fetchFn: (page?: number, limit?: number) => Promise<ProductResponse>
+  fetchFn: (page?: number, limit?: number) => Promise<ProductResponse>,
+  page: number = 0,
+  limit: number = 10
 ) {
   const [data, setData] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -12,7 +14,7 @@ export default function useFetchProducts(
     async function fetchProducts() {
       setIsLoading(true);
       try {
-        const data = await fetchFn(1, 1);
+        const data = await fetchFn(page, limit);
         console.log(data);
         setData(data.products);
       } catch (err) {
