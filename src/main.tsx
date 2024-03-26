3b86286915db1f537bc4243a8fbb634d24345132
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-26 23:02:09 +0100

    add(redux): wrap app inside redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { enableMockServiceWorker } from "./mocks/browser.ts";
 
+import { store } from "./store/store.ts";
+import { Provider } from "react-redux";
+
 enableMockServiceWorker().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   );
 });
