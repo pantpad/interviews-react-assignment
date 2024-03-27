989599effd16bc428acae725e09dc827d65e71d0
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-27 12:10:01 +0100

    feat(cartDrawer): show all cart items

--- src/components/CartDrawer.tsx
+++ src/components/CartDrawer.tsx
@@ -5,5 +5,13 @@ import { useAppSelector } from "../store/hooks";
 export default function CartDrawer() {
   const cart = useAppSelector((state) => state.cart.value);
   console.log(cart.items);
-  return <Box p={2} sx={{ minWidth: "300px" }}></Box>;
+  return (
+    <Box p={2} sx={{ minWidth: "300px" }}>
+      {cart.items.map((item) => (
+        <Box key={item.product.id}>
+          {item.product.name} x{item.quantity}
+        </Box>
+      ))}
+    </Box>
+  );
 }
